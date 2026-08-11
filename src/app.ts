b399acd825e45b66c24f3f7375cbad77d141232d
GuilhermeVRocha <guilhermevieirar14@gmail.com>
2026-08-11 14:24:36 -0300

    change vercel.json and add origin in cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import cors from 'cors';
-import express from 'express';
+import cors from 'cors'
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(
+  cors({ origin: 'https://guilherme-v-rocha.github.io/guilhermevieira/' }),
+)
 
-app.use(express.json());
+app.use(express.json())
 
-export { app };
+export { app }

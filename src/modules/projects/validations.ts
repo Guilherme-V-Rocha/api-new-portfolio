4729c8c35df7c03f08c72d78aae6b407634799e9
GuilhermeVRocha <guilhermevieirar14@gmail.com>
2026-08-11 10:16:30 -0300

    fix schema

--- src/modules/projects/validations.ts
+++ src/modules/projects/validations.ts
@@ -1,10 +1,14 @@
 import z from 'zod'
 
 export const projectSchema = z.object({
-  name: z.string().min(1, { error: 'O nome do projeto é obrigatório' }),
-  description: z
+  title: z.string().min(6, { error: 'O título do projeto é obrigatório' }),
+  shortDescription: z
     .string()
-    .min(1, { error: 'A descrição do projeto é obrigatória' }),
-  image: z.url({ error: 'A imagem do projeto deve ser uma URL válida' }),
-  link: z.url({ error: 'O link do projeto deve ser uma URL válida' }),
+    .min(10, { error: 'A descrição do projeto é obrigatória' }),
+  highlights: z
+    .array(z.string())
+    .min(1, { error: 'Os destaques do projeto são obrigatórios' }),
+  type: z.string().min(4, { error: 'O tipo do projeto é obrigatório' }),
+  link: z.url({ error: 'O link do projeto é obrigatório' }),
+  stacksIds: z.array(z.number()).optional(),
 })
